90de94277b1bae6d8538127af5cedd16e7bdb0ad
trinityvion <89361271+trinityvion@users.noreply.github.com>
2021-08-26 20:53:34 -0400

    ext. script

---
+++ copy.js
@@ -0,0 +1,9 @@
+function copy(that){
+var inp =document.createElement('input');
+document.body.appendChild(inp)
+inp.value =that.textContent
+inp.select();
+document.execCommand('copy',false);
+inp.remove();
+alert("Copied to clipboard");
+}
